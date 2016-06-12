23f14b4a548d441ab1ea3358223683c8f9c9649e
Michael Pfister <pfista@gmail.com>
2016-06-11 21:54:38 -0700

    Parse CLI arguments
    Removes hardcoded record in favor of CLI argument

--- update.go
+++ update.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/docopt/docopt-go"
 	"io/ioutil"
 	"net/http"
 	"os"
@@ -16,34 +17,38 @@ const HOST = "https://api.dreamhost.com/"
 var KEY = os.Getenv("DREAMHOST_DNS_API_KEY")
 
 func main() {
-	ip := ""
+	usage := `Dreamhost Dynamic DNS Updater.
+
+Usage:
+  dreamhost-ddns <record>
+
+Options:
+  -h --help     Show this screen.
+  --version     Show version.`
+
+	arguments, _ := docopt.Parse(usage, nil, true, "0.1.0", false)
+
+	record, _ := arguments["<record>"].(string)
+	dnstype := "A"
+
+	// Get the current value for the dns record
+	var ip string
 	records := getRecords()
 	for _, e := range records.Data {
-		if e.Type == "A" && e.Record == "home.pfista.io" {
+		if e.Type == dnstype && e.Record == record {
 			ip = e.Value
 		}
 	}
 
-	t := time.NewTicker(5 * time.Second)
-	for now := range t.C {
-		fmt.Println("Checking DNS at", now)
+	// If the ip address is different than the current dns record, update it
+	t := time.NewTicker(30 * time.Minute)
+	for range t.C {
 		var latestIp = getIp()
 		if latestIp != ip {
 			ip = latestIp
-			updateDNS(ip)
+			updateDNS(record, dnstype, ip)
 		}
 	}
-}
-
-func updateDNS(ip string) {
-	records := getRecords()
-	for _, e := range records.Data {
-		if e.Type == "A" && e.Record == "home.pfista.io" {
-			fmt.Println("Removing DNS A entry")
-			removeRecord("home.pfista.io", "A", e.Value)
-		}
-	}
-	addRecord("home.pfista.io", "A", ip)
 }
 
 func getIp() string {
@@ -54,31 +59,6 @@ func getIp() string {
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 	return strings.TrimSpace(string(body))
-}
-
-func getRecords() DreamhostResponse {
-	req, _ := http.NewRequest("GET", HOST, nil)
-	q := req.URL.Query()
-	q.Add("cmd", "dns-list_records")
-	q.Add("unique_id", uuid())
-	q.Add("format", "json")
-	q.Add("key", KEY)
-	req.URL.RawQuery = q.Encode()
-	fmt.Println(req.URL.String())
-
-	client := &http.Client{}
-
-	resp, err := client.Do(req)
-	if err != nil {
-		fmt.Printf("%s\n", err)
-	}
-	defer resp.Body.Close()
-
-	body, err := ioutil.ReadAll(resp.Body)
-
-	res := DreamhostResponse{}
-	json.Unmarshal([]byte(body), &res)
-	return res
 }
 
 func uuid() string {
@@ -105,6 +85,45 @@ type DnsRecord struct {
 	Zone      string `json:"zone"`
 }
 
+func updateDNS(record, dnstype, value string) {
+	records := getRecords()
+	var update = false
+	for _, e := range records.Data {
+		if e.Type == dnstype && e.Record == record && e.Value != value {
+			removeRecord(record, dnstype, e.Value)
+			update = true
+		}
+	}
+	if update {
+		addRecord(record, dnstype, value)
+	}
+}
+
+func getRecords() DreamhostResponse {
+	req, _ := http.NewRequest("GET", HOST, nil)
+	q := req.URL.Query()
+	q.Add("cmd", "dns-list_records")
+	q.Add("unique_id", uuid())
+	q.Add("format", "json")
+	q.Add("key", KEY)
+	req.URL.RawQuery = q.Encode()
+	//fmt.Println(req.URL.String())
+
+	client := &http.Client{}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Printf("%s\n", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	res := DreamhostResponse{}
+	json.Unmarshal([]byte(body), &res)
+	return res
+}
+
 func addRecord(record, dnstype, value string) {
 	req, _ := http.NewRequest("GET", HOST, nil)
 	q := req.URL.Query()
@@ -118,7 +137,7 @@ func addRecord(record, dnstype, value string) {
 	q.Add("value", value)
 
 	req.URL.RawQuery = q.Encode()
-	fmt.Println(req.URL.String())
+	//fmt.Println(req.URL.String())
 
 	client := &http.Client{}
 
@@ -132,10 +151,10 @@ func addRecord(record, dnstype, value string) {
 	res := DreamhostResponse{}
 	json.Unmarshal([]byte(body), &res)
 
-	fmt.Printf("data %s, response %s\n", res.Data, res.Result)
-
-	if res.Result != "success" {
-		fmt.Println(res.Result)
+	if res.Result == "success" {
+		fmt.Printf("Successfully added dns record: %s, type: %s, value: %s\n", record, dnstype, value)
+	} else {
+		fmt.Printf("Error adding dns record: %s, type: %s, value: %s\n", record, dnstype, value)
 		fmt.Println(res.Data)
 	}
 }
@@ -153,7 +172,7 @@ func removeRecord(record, dnstype, value string) {
 	q.Add("value", value)
 
 	req.URL.RawQuery = q.Encode()
-	fmt.Println(req.URL.String())
+	//fmt.Println(req.URL.String())
 
 	client := &http.Client{}
 
@@ -167,11 +186,10 @@ func removeRecord(record, dnstype, value string) {
 	res := DreamhostResponse{}
 	json.Unmarshal([]byte(body), &res)
 
-	fmt.Printf("data %s, response %s\n", res.Data, res.Result)
-
-	if res.Result != "success" {
-		fmt.Println(res.Result)
+	if res.Result == "success" {
+		fmt.Printf("Successfully removed dns record: %s, type: %s, value: %s\n", record, dnstype, value)
+	} else {
+		fmt.Printf("Error removing dns record: %s, type: %s, value: %s\n", record, dnstype, value)
 		fmt.Println(res.Data)
 	}
-
 }
